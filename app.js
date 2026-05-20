c54189143dd3ccc1941fad718bb38c07b5cfc37d
Naveen <naveenadhikari011@gmail.com>
2026-05-20 10:16:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>🚀 hey there ! App working </h1>
+    <p>Version: <strong>1.0.0</strong></p>
+
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
